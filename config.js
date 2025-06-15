1c0567c166582edf69b7d646d779c7aeb81080d5
llll12t <147978645+llll12t@users.noreply.github.com>
2025-06-15 16:18:02 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,12 @@
+
+// config.js
+const CONFIG = {
+    LIFF_ID: '2007514355-N7ZqG4BK', // **สำคัญมาก: ใส่ LIFF ID ของคุณที่นี่ (อันเดิมที่เคยใช้)**
+
+    // URL ของ Google Cloud Functions ที่คุณ Deploy ได้รับมา
+    CLOUD_FUNCTION_GET_URL: 'https://us-central1-crm-3rn.cloudfunctions.net/apiGet',
+    CLOUD_FUNCTION_POST_URL: 'https://us-central1-crm-3rn.cloudfunctions.net/apiPost',
+};
+
+// ถ้าใช้ ES Module ให้เพิ่ม export:
+// export default CONFIG;
